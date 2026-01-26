6becfe89f1f248938c718d2780e94963a755a6e0
Camilo8902 <ernesto.rodriguez120289@gmail.com>
2026-01-25 22:26:42 -0500

    Signed-off-by: Camilo8902 <ernesto.rodriguez120289@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, Suspense } from 'react';
+import { useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
